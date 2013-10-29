029f3aa4f6aca5c8debb7676cff83d81aab36739
erikfromm <erik.from@live.com>
2013-10-29 17:56:34 -0300

    apriendiendo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MediaGO::Application.config.secret_key_base = '0f34d41232daf9f6a10ec5094782660b9ea56663d331b42b1c5bad58d0072c2f26e855c2d5b6666376ceaf75910706bfe6d80e6153a510d0e0b089911dc40fc5'
