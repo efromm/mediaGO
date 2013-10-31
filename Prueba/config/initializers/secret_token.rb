b3ef46d4ca59c2692a4c76822b15be24f2ecdc87
efromm <erik.from@live.com>
2013-10-31 00:09:36 -0300

    borrando Prueba

--- Prueba/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Prueba::Application.config.secret_token = '3de35ce3faf59fe4e0c357615a6a068b26dcabf7454f17fb3a018b3a4b2747175c33839d483947824cc017fe388d279df3956a3dcd153fabea8a959654b5ece5'
